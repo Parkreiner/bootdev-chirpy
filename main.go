6b0fa8ce8153d71e5412ccd0f9e136ca4fabea28
Michael Smith <throwawayclover@gmail.com>
2024-12-21 22:24:59 -0500

    wip: commit progress on chapter 2

--- main.go
+++ main.go
@@ -4,9 +4,37 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync/atomic"
 )
 
 const portNumber = 8080
+
+type apiConfig struct {
+	fileserverHits atomic.Uint32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	c.fileserverHits.Add(1)
+	return next
+}
+
+func (c *apiConfig) hitsSinceLastShutdown(
+	w http.ResponseWriter,
+	_ *http.Request,
+) {
+	headers := w.Header()
+	headers.Set("Cache-Control", "no-cache")
+
+	hits := c.fileserverHits.Load()
+	msg := "Hits: " + strconv.Itoa(int(hits))
+	headers.Set("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(msg))
+}
+
+func (c *apiConfig) resetHits(w http.ResponseWriter, _ *http.Request) {
+	c.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -15,9 +43,15 @@ func main() {
 		Handler: mux,
 	}
 
+	apiCfg := apiConfig{}
 	mux.Handle(
 		"/app/",
-		http.StripPrefix("/app", http.FileServer(http.Dir("."))),
+		http.StripPrefix(
+			"/app",
+			apiCfg.middlewareMetricsInc(
+				http.FileServer(http.Dir(".")),
+			),
+		),
 	)
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
@@ -29,6 +63,9 @@ func main() {
 			log.Printf("Unable to write response to header. Error: %v\n", err)
 		}
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.hitsSinceLastShutdown)
+	mux.HandleFunc("/reset", apiCfg.resetHits)
 
 	log.Fatal(server.ListenAndServe())
 }
